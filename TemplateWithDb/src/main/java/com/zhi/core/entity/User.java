91b79b43ee2bb443b8af932f610f8695a4e33f9e
支丹枫 <danfeng.zhi@vantopgroup.com>
2021-03-24 15:14:13 +0800

    增加Mybatis-Plus模板service的改造

---
+++ TemplateWithDb/src/main/java/com/zhi/core/entity/User.java
@@ -0,0 +1,7 @@
+package com.zhi.core.entity;
+
+import java.io.Serializable;
+
+public class User extends BaseEntity implements Serializable {
+
+}
---
+++ TemplateWithDb/src/main/java/com/zhi/service/UserService.java
@@ -0,0 +1,10 @@
+package com.zhi.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zhi.core.entity.User;
+
+import java.util.List;
+
+public interface UserService extends IService<User> {
+    List<User> listAll();
+}
